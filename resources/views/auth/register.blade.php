4fd5a582abb643148f71a13fafe9e0fe99812284
ekyeky <ekyeky1019@icloud.com>
2020-06-18 06:00:02 +0000

    kadai-tasklist2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="text-center">
-        <h1>Sign up</h1>
+        <h1>ユーザー登録</h1>
     </div>
     
     <div class="row">
@@ -10,25 +10,25 @@
             
             {!! Form::open(["route" => "signup.post"]) !!}
                 <div class="form-group">
-                    {!! Form::label('name', 'Name') !!}
+                    {!! Form::label('name', '氏名') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
-                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::label('email', "メールアドレス") !!}
                     {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
-                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::label('password', "パスワード") !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
-                    {!! Form::label('password_confirmation', 'Confirmation') !!}
+                    {!! Form::label('password_confirmation', "パスワード確認用") !!}
                     {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
-                 {!! Form::submit("signup", ['class' => 'btn btn-primary btn-block']) !!}
+                 {!! Form::submit("新規登録", ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
         </div>
     </div>
